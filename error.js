c4a0bdd82d9a56ab5d2dec571c4b8a8f0fcade29
furkle <furkle@furkleindustries.com>
2019-02-16 20:39:56 -0500

    Adding some fallbacks for error.

--- error.js
+++ error.js
@@ -1,17 +1,13 @@
 const chalk = require('chalk');
 const processLogLine = require('./processLogLine');
-const {
-  inspect: { styles },
-} = require('util');
 
 module.exports = function error(value, colorFormatter, bgColorFormatter) {
-  
   if (value instanceof Error) {
     console.error(processLogLine(
-      value.stack,
+      value.stack || value.message || value,
       colorFormatter || chalk.red,
       bgColorFormatter,
-      ));
+    ));
   } else {
     console.error(processLogLine(
       value,
