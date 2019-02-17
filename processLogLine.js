f000ab1277ca1f4163f30d43eca83a364a71b621
furkle <furkle@furkleindustries.com>
2019-02-16 21:57:01 -0500

    Adding custom handling of objects in node and browser.

--- processLogLine.js
+++ processLogLine.js
@@ -1,5 +1,15 @@
+const isNode = require('./isNode');
+
 module.exports = function processLogLine(value, colorFormatter, bgColorFormatter) {
   let output = value;
+  if (output && (Array.isArray(output) || typeof output === 'object')) {
+    if (isNode()) {
+      output = require('util').inspect(value);
+    } else {
+      output = JSON.stringify(output, null, 2);
+    }
+  }
+
   if (typeof colorFormatter === 'function') {
     output = colorFormatter(output);
   }
