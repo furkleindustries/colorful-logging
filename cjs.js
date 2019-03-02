edff019a6fdb718333e262ee02b772c4e7fb5fac
furkle <furkle@furkleindustries.com>
2019-03-02 01:13:58 -0500

    Adding the proper file properties in package.json.

---
+++ cjs.js
@@ -0,0 +1,6 @@
+module.exports = {
+  error: require('./error'),
+  log: require('./log'),
+  processLogLine: require('./processLogLine'),
+  warn: require('./warn'),
+};
--- index.d.ts
+++ index.d.ts
@@ -4,6 +4,5 @@ import {
 
 export function error(value: any, colorFormatter?: Chalk, bgColorFormatter?: Chalk): void;
 export function log(value: any, colorFormatter?: Chalk, bgColorFormatter?: Chalk): void;
-export function warn(value: any, colorFormatter?: Chalk, bgColorFormatter?: Chalk): void;
 export function processLogLine(value: any, colorFormatter?: Chalk, bgColorFormatter?: Chalk): string;
-
+export function warn(value: any, colorFormatter?: Chalk, bgColorFormatter?: Chalk): void;
